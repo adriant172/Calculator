c07405fa16be7fd1fa3789df4a549db54475dee2
Matthew Thomas <adriant172@gmail.com>
2022-05-20 00:11:58 +0900

    create operate function

--- scripts.js
+++ scripts.js
@@ -22,3 +22,16 @@ function multiply(array) {
 function divide(dividend, divisor) {
     return dividend / divisor;
 }
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
