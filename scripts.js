eb849881bae92c442c881a3f388c0f4b74e7ef98
Matthew Thomas <adriant172@gmail.com>
2022-05-21 22:09:59 +0900

    Create createCalculator function

--- scripts.js
+++ scripts.js
@@ -20,9 +20,20 @@ function operate(operator, num1, num2) {
             return add(num1, num2);
         case "-":
             return subtract(num1, num2);
-        case "*":
+        case "·":
             return multiply(num1, num2);
-        case "/":
+        case "÷":
             return divide(num1, num2);
     }
+}
+
+function createCalculator() {
+    const buttonValues = [1,2,3,"+",4,5,6,"-",7,8,9,"·", "C", 0, "=","÷"];
+    const calculator = document.querySelector('.calculator');
+    for (value of buttonValues) {
+        let calculatorButton = document.createElement('div');
+        calculatorButton.classList.add('calButton');
+        calculator.appendChild(calculatorButton);
+    }
+
 }
